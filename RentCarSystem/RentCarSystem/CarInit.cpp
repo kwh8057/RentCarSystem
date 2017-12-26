ad50ea24a9294646f8f6727ff442ab32bf868600
kwh8057 <kdk7030@gmail.com>
2017-12-27 08:05:33 +0900

    carList에 car4 추가
    car4가 추가 됨

--- RentCarSystem/RentCarSystem/CarInit.cpp
+++ RentCarSystem/RentCarSystem/CarInit.cpp
@@ -3,13 +3,15 @@
 list<Car> CarInit() {
 	list<Car> * carList = new list<Car>();
 
-	Car * car1 = new Car(1, 5000, "Sonata", "Çö´ë", "BLACK");
+	Car * car1 = new Car(1, 5000, "Sonata", "Ã‡Ã¶Â´Ã«", "BLACK");
 	Car * car2 = new Car(2, 7000, "K5", "KIA", "GREY");
-	Car * car3 = new Car(3, 10000, "±×·£Àú", "Çö´ë", "WHITE");
+	Car * car3 = new Car(3, 10000, "Ä…Ã—Ë‡ÅÅ”Ãº", "Ã‡Ã¶Â´Ã«", "WHITE");
+	Car * car4 = new Car(4, 3000, "Avante", "Hyundai", "BLACK");
 
 	carList->push_back(*car1);
 	carList->push_back(*car2);
 	carList->push_back(*car3);
+	carList->push_back(*car4);
 
 	return *carList;
 }
