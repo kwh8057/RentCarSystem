b7afa92a54f08da4f505a04b38978efab2d750d7
Kim Woo Hyun <kdk7030@gmail.com>
2017-12-28 15:15:53 +0900

    쓸데없는 주석 삭제

--- RentCarSystem/RentCarSystem/CarIndex.cpp
+++ RentCarSystem/RentCarSystem/CarIndex.cpp
@@ -7,10 +7,9 @@
 #include "SetColor.h"
 #include "CarInit.h"
 
-list<Car>::iterator CAR_DISPLAY::Menu(int flag, list<Car> & carList)//화면에 보여지는 것들은 이 클래스에서 만들어질것입니다.
+list<Car>::iterator CAR_DISPLAY::Menu(int flag, list<Car> & carList)
 {
 	int i, j = 0, x, y, yy, ch, num = 1;
-	//list<Car> * carListMenu = new list<Car>();
 
 	system("mode con: lines=25 cols=65"); //화면의 크기를 조절합니다.
 	system("cls");//전체 화면을 깨끗하게 지웁니다.
@@ -29,10 +28,10 @@ list<Car>::iterator CAR_DISPLAY::Menu(int flag, list<Car> & carList)//화면에 보�
 			case UP: y -= 1; break;
 			case DOWN: y += 1; break;
 			}
-			if (y >= j + yy) y = yy; //메뉴의 7번 아래로 누르면 1번으로
-			else if (y < yy) y = yy + j - 1;//1번 위로 누르면 7번으로
+			if (y >= j + yy) y = yy;
+			else if (y < yy) y = yy + j - 1;
 
-			num = y;//한칸씩 띄어서 작성되었으니 번호는 2로 나누어야한다.
+			num = y;
 			printMenu(num, yy, j, carList); //선택이 바뀌었으니 컬러변경을 위해서 다시 출력
 		}
 		else if (ch == ESC) {
@@ -46,7 +45,6 @@ list<Car>::iterator CAR_DISPLAY::Menu(int flag, list<Car> & carList)//화면에 보�
 		}
 		else continue;
 	}
-	//return ch - '0'; //입력받는게 문자이므로 숫자로 바꾸어서 리턴한다.
 }
 
 void CAR_DISPLAY::printMenu(int n, int &y, int &j, list<Car> & carList)
