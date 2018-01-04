6e20d7d7acd8cc2f162f048984bf2a2e4ccc08ba
우현 김 <vega_x@naver.com>
2018-01-04 13:11:06 +0900

    GREY -> GRAY

--- RentCarSystem/RentCarSystem/CarInit.cpp
+++ RentCarSystem/RentCarSystem/CarInit.cpp
@@ -4,7 +4,7 @@ list<Car> CarInit() {
 	list<Car> * carList = new list<Car>();
 
 	Car * car1 = new Car(1, 5000, "Sonata", "HYUNDAI", "BLACK");
-	Car * car2 = new Car(2, 7000, "K5", "KIA", "GREY");
+	Car * car2 = new Car(2, 7000, "K5", "KIA", "GRAY");
 	Car * car3 = new Car(3, 10000, "Grandeur", "HYUNDAI", "WHITE");
 	Car * car4 = new Car(4, 3000, "Avante", "Hyundai", "BLACK");
 
